ac06ec3ce0f95913edbd2d5f3b626e18786b0881
Poul Williams <poul@paweng.com>
2017-10-29 13:42:50 -0400

    Update to Package.swift: Added targets

--- Package.swift
+++ Package.swift
@@ -1,6 +1,11 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 /*
  * Package.swift
  * Copyright (c) 2015 Ben Gollmer.
+ *
+ * 2017/10/29 by Poul Williams: Updated for Swift 4
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +24,12 @@ import PackageDescription
 
 let package = Package(
   name: "CommandLine",
-  target: [],
+  targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "CommandLine",
+            dependencies: []),
+  ],
   exclude: ["script"]
 )
