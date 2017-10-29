469fb9b544f4fc6d74efd05558a0a952d166581e
Poul Williams <poul@paweng.com>
2017-10-29 13:47:04 -0400

    Update to Package.swift

--- Package.swift
+++ Package.swift
@@ -24,7 +24,6 @@ import PackageDescription
 
 let package = Package(
   name: "CommandLine",
-  exclude: ["script"],
   targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
